6bdd863f93ec1983c14082e3eaebd33cecd89036
Usman <55032537+usmanashraf804@users.noreply.github.com>
2025-02-11 05:12:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
+    "🥺",
+    "sochlo",
+    "please?",
     "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
+    "jaan pleaseee 🥺",
+    "you say no i go cry :(",
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "say yes 😌"
 ];
 
 let messageIndex = 0;
